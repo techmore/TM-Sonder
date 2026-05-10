5da01279cccd1dc9c58f16ca7c9dee17750c4866
techmore.co <sdolbec1@gmail.com>
2026-05-09 22:00:22 -0400

    Initial Commit

---
+++ xcode-TM-SonderTests/xcode_TM_SonderTests.swift
@@ -0,0 +1,19 @@
+//
+//  xcode_TM_SonderTests.swift
+//  xcode-TM-SonderTests
+//
+//  Created by techmore on 5/9/26.
+//
+
+import Testing
+@testable import xcode_TM_Sonder
+
+struct xcode_TM_SonderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
