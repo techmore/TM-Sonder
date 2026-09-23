40e71b53319fc056a4eff7423f63b62830fb4b1d
Sean Dolbec <sean@example.com>
2026-09-23 10:36:42 -0400

    point homebrew formula at v0.2.1

--- Formula/tm-sonder.rb
+++ Formula/tm-sonder.rb
@@ -1,9 +1,9 @@
 class TmSonder < Formula
   desc "Personal media and audiobook server"
   homepage "https://github.com/techmore/TM-Sonder"
-  url "https://github.com/techmore/TM-Sonder/archive/refs/tags/v0.2.0.tar.gz"
-  version "0.2.0"
-  sha256 "38d47c87edf054904113c5bfd339aa91e6178dbe2784e7fee15f6a6e3a9278c8"
+  url "https://github.com/techmore/TM-Sonder/archive/refs/tags/v0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "73ff168ff06b051f1c57d5d222b623b9a81bbbc7f2c68a55d03e9809dc668962"
 
   depends_on "go" => :build
   depends_on "ffmpeg"
